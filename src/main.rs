46f26551dff5d039b40df398b29edb03365a3829
Utishnik <yrik.kokoyto.00@bk.ru>
2026-02-02 00:04:45 +0300

    micro server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use samotop::io::DummyService;
+use samotop::server::TcpServer;
+use tokio;
+use std::error::Error;
 fn main() {
-    println!("Hello, world!");
+    let rt: tokio::runtime::Runtime = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+        let mut srv: Result<(), Box<dyn Error + Send + Sync>> =
+            TcpServer::on("127.0.0.1:2525").serve(DummyService).await;
+    });
 }

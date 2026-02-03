a026da96ba78cdb1aeb7e0dccf0fa5517c809731
Utishnik <yrik.kokoyto.00@bk.ru>
2026-02-04 00:26:05 +0300

    заменил на async std

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use samotop::io::DummyService;
 use samotop::server::TcpServer;
-use tokio;
-use std::error::Error;
 fn main() {
-    let rt: tokio::runtime::Runtime = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(async {
-        let mut srv: Result<(), Box<dyn Error + Send + Sync>> =
-            TcpServer::on("127.0.0.1:2525").serve(DummyService).await;
-    });
+    println!("Server start");
+    let srv =
+        TcpServer::on("127.0.0.1:2525").serve(DummyService);
+    async_std::task::block_on(srv).unwrap();
+    println!("Server close");
 }
